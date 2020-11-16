3ca61554a678b0773f66ff5873915292834225ce
Sebastian Sicardi <smcsicardi@gmail.com>
2020-11-16 18:54:54 -0300

    cleanup

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('unique/', views.getUniqueEvents, name='getUniqueEvents'),
-	path('histogram/<str:event>/<str:date>/', views.histogramFromDate, name='histogramFromDate'),
-	path('<str:event>/', views.countEvent, name='countEvent'),
+    path('unique/', views.getUniqueEvents, name='getUniqueEvents'),
+    path('histogram/<str:event>/<str:date>/', views.histogramFromDate, name='histogramFromDate'),
+    path('<str:event>/', views.countEvent, name='countEvent'),
     path('<str:event>/<int:count>/', views.countEvent, name='countEvent'),
     path('', views.getEventsFromDates, name='getEventsFromDates'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,95 +1,95 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.utils import timezone
-from datetime import datetime
-from datetime import timedelta
+from django.db.models import Count
+from datetime import datetime, timedelta
 from django.views.decorators.csrf import csrf_exempt
-from django.db.models import F
 
 from .models import Events
-from django.db.models import Count
 
 import io
 import matplotlib.pyplot as plt
-from django.http import HttpResponse
-from django.shortcuts import render
+
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 import matplotlib
 
 try:
-	matplotlib.use("TKAgg")
+    matplotlib.use("TKAgg")
 except ModuleNotFoundError:
-	matplotlib.use('Agg')
+    matplotlib.use('Agg')
+
 
 @csrf_exempt
 def countEvent(request, event, count=1):
-	t = timezone.now()
-	if count == 1:
-		event_instance = Events.objects.create(event_type=event, date=t)
-	else:
-		events_list = [Events(event_type=event, date=t) for i in range(count)] 
-		Events.objects.bulk_create(events_list)
-	return JsonResponse({"Ok":"Ok"})
+    t = timezone.now()
+    if count == 1:
+        Events.objects.create(event_type=event, date=t)
+    else:
+        events_list = [Events(event_type=event, date=t) for i in range(count)]
+        Events.objects.bulk_create(events_list)
+
 
 def getUniqueEvents(request):
-	try:
-		
-		queryResult = list(Events.objects.values('event_type').annotate(count=Count('id')))
+    try:
+        queryResult = list(Events.objects.values('event_type').annotate(count=Count('id')))
 
-		jsonRes = {"result": [{"event":d["event_type"], "count": d["count"]} for d in queryResult]}
+        jsonRes = {"result": [{"event": d["event_type"], "count": d["count"]} for d in queryResult]}
 
-		return JsonResponse(jsonRes)
-	except Exception as e:
-		return JsonResponse({"error": str(e)})
+        return JsonResponse(jsonRes)
+    except Exception as e:
+        return JsonResponse({"error": str(e)})
+
 
 def getEventsFromDates(request):
-	try:
-		start = datetime.strptime(request.GET['start_date'], "%Y%m%d%H")
-		end =  datetime.strptime(request.GET['end_date'], "%Y%m%d")
+    try:
+        start = datetime.strptime(request.GET['start_date'], "%Y%m%d%H")
+        end = datetime.strptime(request.GET['end_date'], "%Y%m%d")
 
-		#we add hours and minutes to END to assure we get the events from the day
-		time_added = timedelta(hours = 23, minutes= 59, seconds= 59)
-		end+= time_added
+        # we add hours and minutes to END to assure we get the events from the day
+        time_added = timedelta(hours=23, minutes=59, seconds=59)
+        end += time_added
 
-		querySet = Events.objects.filter(date__range=(start, end))
-		queryResult = list(querySet.values('date__date', 'event_type'))
+        querySet = Events.objects.filter(date__range=(start, end))
+        queryResult = list(querySet.values('date__date', 'event_type'))
 
-		jsonDict = {"result": [{"date": str(d["date__date"]), "event":d["event_type"], "count": d["count"]} for d in queryResult]}
+        jsonDict = {"result": [{"date": str(d["date__date"]), "event": d["event_type"], "count": d["count"]} for d in
+                               queryResult]}
 
-		return JsonResponse(jsonDict)
-	except Exception as e:
-		return JsonResponse({"error": str(e)})
+        return JsonResponse(jsonDict)
+    except Exception as e:
+        return JsonResponse({"error": str(e)})
+
 
 def histogramFromDate(request, event, date):
-	try:
-		start = datetime.strptime(date, "%Y%m%d")
-		time_added = timedelta(hours = 23, minutes= 59, seconds= 59)
-		end = start + time_added
+    try:
+        start = datetime.strptime(date, "%Y%m%d")
+        time_added = timedelta(hours=23, minutes=59, seconds=59)
+        end = start + time_added
 
-		querySet = Events.objects.filter(date__range=(start, end) , event_type=event).values()
-		queryResult = list(querySet.values('date__hour').annotate(count=Count('id')))
+        querySet = Events.objects.filter(date__range=(start, end), event_type=event).values()
+        queryResult = list(querySet.values('date__hour').annotate(count=Count('id')))
 
-		jsonDict = {"hour": [d["date__hour"] for d in queryResult], "count": [d["count"] for d in queryResult]}
+        jsonDict = {"hour": [d["date__hour"] for d in queryResult], "count": [d["count"] for d in queryResult]}
 
-		print(jsonDict)
+        print(jsonDict)
 
-		return generateImage(jsonDict, event, date)
-		#return JsonResponse(jsonDict)
-	except Exception as e:
-		return JsonResponse({"error": str(e)})
+        return generateImage(jsonDict, event, date)
+    except Exception as e:
+        return JsonResponse({"error": str(e)})
+
 
 def generateImage(jsonDict, event, date):
     f = plt.figure()
 
-    plt.bar(jsonDict["hour"], [x/sum(jsonDict["count"])*100 for x in jsonDict["count"]])
+    plt.bar(jsonDict["hour"], [x / sum(jsonDict["count"]) * 100 for x in jsonDict["count"]])
 
     plt.xlabel("Hour")
     plt.ylabel("Frecuency (%)")
-    plt.xlim((0,24))
-    plt.ylim((0,100))
+    plt.xlim((0, 24))
+    plt.ylim((0, 100))
 
     plt.title("Event: {} by hour on {}".format(event, date))
     time = [i for i in range(24)]
-    plt.xticks(time,time)
+    plt.xticks(time, time)
 
     buf = io.BytesIO()
     canvas = FigureCanvasAgg(f)
@@ -100,17 +100,3 @@ def generateImage(jsonDict, event, date):
     response['Content-Length'] = str(len(response.content))
 
     return response
-
-
-
-
-
-
-
-
-
-
-
-
-
-
